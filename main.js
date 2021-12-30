87de530ac46b468d7c5e6b05b4a77e81826f56df
Hrishit-Gadia <80762914+Hrishit-Gadia@users.noreply.github.com>
2021-12-30 10:23:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -115,7 +115,6 @@ function test() {
     localStorage.setItem("Number", qwerty);
 }
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
