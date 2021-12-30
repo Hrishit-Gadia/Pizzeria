2f6cf8c4b018a377c5d930652a9cc2ef0711ff6d
Hrishit-Gadia <80762914+Hrishit-Gadia@users.noreply.github.com>
2021-12-30 10:17:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -114,3 +114,13 @@ function test() {
     qwerty = position_list_array.length
     localStorage.setItem("Number", qwerty);
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
