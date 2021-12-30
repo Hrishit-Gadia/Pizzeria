d45facfa9f83d2adac0a609efae79b0aeceb5a4e
Hrishit-Gadia <80762914+Hrishit-Gadia@users.noreply.github.com>
2021-12-30 10:24:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -115,11 +115,3 @@ function test() {
     localStorage.setItem("Number", qwerty);
 }
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
